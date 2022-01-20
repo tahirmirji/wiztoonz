09ee1e910c76b78d5832fb376fdd665d76156460
TAHIR <34068623+tahirmirji@users.noreply.github.com>
2022-01-20 12:30:12 +0530

    First Changes

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+// define("SITE_URL","www.google.com");
+// $number1=10;
+// $number2=20;
+// $result=0;
+global $number1;
+// $result = $number1 + $number2 ;
+
+// echo "Addition of $number1 and $number2 is $result <br/> Best search engine is ".SITE_URL;
+
+class A
+{
+   public $a = 10;
+
+}
+
+function add( $a,$c)
+{
+   $b=10; 
+  
+
+
+   echo " $number1 , $b , $a, $c ";
+
+
+}
+
+
+add(100,200);
+
+/*
+ echo "<h1>Welcome to the world of <i> Dynamic Websites</i></h1>";
+*/
+
+?>
---
+++ index1.php
@@ -0,0 +1,26 @@
+
+
+<?php
+// BODMAS
+// Brackets of div Mul Add Sub
+// Ex: Answer = 4 - (10/2-2*2)
+// Answer = 4 - (5-2*2)
+// Answer = 4 - (5-4)
+// Answer = 4 - (1)
+// Answer = 3
+
+
+    $variable = "name";
+    $literally = "My $variable will not print";
+    print($literally); print("<br/>");
+
+    $literally = "My $variable will print";
+    print($literally); print("<br/>");
+
+    $literally = 10;
+    print($literally); print("<br/>");
+
+    $literally = 3.142;
+    print($literally); print("<br/>");
+
+?>
